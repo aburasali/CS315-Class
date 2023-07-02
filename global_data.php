85376deb0cb1f1435f7b08cd19011f7418e992eb
aburasali <aburasali@gmail.com>
2023-07-02 10:25:54 +0200

    deleted all extra files

--- global_data.php
+++
@@ -1,7 +0,0 @@
-<!-- /* Name: Mohammed Ibrahim
-date: 11/6/2023
-array to save loclal database
-*/ -->
-<?php
-global $my_data;
-$my_data = array('admin_2023' => 'Admin_2023', 'cs314_2023' => 'Cs314_2023', 'system_admin' => 'System_admin1');
